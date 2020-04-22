bfa496eda8d6108c1f84c661e19dc1dc26c8c118
Anand <anand.srivastav@oaktreecloud.com>
2020-04-22 16:19:44 +0530

    creating users from csv

--- application_spec.rb
+++ application_spec.rb
@@ -9,8 +9,6 @@ require 'csv'
 describe "User Sign In Scenario" do
 
   before(:each) do
-    # Create user
-    @user = build(:user)
     # Open chrome browser
     @driver = Selenium::WebDriver.for :chrome
     # Describe base URL
@@ -22,20 +20,30 @@ describe "User Sign In Scenario" do
   end
 
   context "Sign in with valid data" do
-    it "User #{@user.username} signed in successfully" do      
-      @driver.get(@base_url + "/login")
-      sleep 10
-      @driver.find_element(name: 'email').send_keys @user.username
-      @driver.find_element(name: 'password').send_keys @user.password
-      @driver.find_element(class: "button-actions").find_element(tag_name: 'input').submit();
-      sleep 10
-      expect(@driver.current_url).to eq "#{@base_url}/role"       
+    
+    users = []
+    CSV.foreach("../rspec/users.csv", headers: true) do |row|
+      user = FactoryBot.create(:user) do |user|
+        user.username = row[1]
+        user.password = row[2]
+      end
+      users << user
+    end    
+
+    users.each do |user|
+      it "User #{user.username} signed in successfully" do      
+        @driver.get(@base_url + "/login")
+        sleep 10
+        @driver.find_element(name: 'email').send_keys user.username
+        @driver.find_element(name: 'password').send_keys user.password
+        @driver.find_element(class: "button-actions").find_element(tag_name: 'input').submit();
+        sleep 10
+        expect(@driver.current_url).to eq "#{@base_url}/role"       
+      end
     end
   end
 
-  after(:each) do
-    @driver.find_element(class: 'hamburger-box').click;
-    @driver.find_element(:xpath,"//a[@id='header_logout__c']").click;    
+  after(:each) do   
     @driver.close();
   end
 
